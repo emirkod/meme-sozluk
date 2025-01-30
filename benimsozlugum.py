1a02f62e3c3db2377bcf301191f70d1ab79bca20
emirkod <cemre1818@hotmail.com>
2025-01-30 20:56:07 +0300

    Update benimsozlugum.py
    daha iyi memeler

--- benimsozlugum.py
+++ benimsozlugum.py
@@ -1,8 +1,12 @@
 meme_dict = {
             "CRINGE": "Garip ya da utandırıcı bir şey",
-            "LOL": "Komik bir şeye verilen cevap",
+            "LOL": "Komik bir şeye verilen cevap(lots of laugh)",
             "NİCE TRY":"İyi denemeydi",
             "EZ WİN": "Kolaydı",
+            "SIGMA BOYS": "ciddi,umursamaz,havalı erkekler"
+            "TROLL FACE": "trollemeyi,başkasını sinirlendirmeyi veya şaka yaptığını belirtmek için kullanılır."
+            "ME GUSTA": "rahatsız edici ama hoşuma gidiyor demektir."
+            "Y U NO GUY": "Birşeyin neden mantıklı olmadığını söylemek için kullanılır."
             }
 
 word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
