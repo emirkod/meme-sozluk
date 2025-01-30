10ae8d75e18e00f5ce20eab4b29559383fc94d29
emirkod <cemre1818@hotmail.com>
2025-01-30 20:30:39 +0300

    Update benimsozlugum.py
    memeler(2)

--- benimsozlugum.py
+++ benimsozlugum.py
@@ -1,6 +1,8 @@
 meme_dict = {
             "CRINGE": "Garip ya da utandırıcı bir şey",
             "LOL": "Komik bir şeye verilen cevap",
+            "NİCE TRY":"İyi denemeydi",
+            "EZ WİN": "Kolaydı",
             }
 
 word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
