262c6b3eb8f23d7b0d66af00c8157cd5c5bac72f
emirkod <cemre1818@hotmail.com>
2025-01-30 21:17:36 +0300

    Update benimsozlugum.py
    Kod düzenlemeleri yapıldı.

--- benimsozlugum.py
+++ benimsozlugum.py
@@ -1,17 +1,16 @@
-meme_dict = {
+while True :
+            meme_dict = {
             "CRINGE": "Garip ya da utandırıcı bir şey",
             "LOL": "Komik bir şeye verilen cevap(lots of laugh)",
             "NİCE TRY":"İyi denemeydi",
             "EZ WİN": "Kolaydı",
-            "SIGMA BOYS": "ciddi,umursamaz,havalı erkekler"
-            "TROLL FACE": "trollemeyi,başkasını sinirlendirmeyi veya şaka yaptığını belirtmek için kullanılır."
-            "ME GUSTA": "rahatsız edici ama hoşuma gidiyor demektir."
+            "SIGMA BOYS": "ciddi,umursamaz,havalı erkekler",
+            "TROLL FACE": "trollemeyi,başkasını sinirlendirmeyi veya şaka yaptığını belirtmek için kullanılır.",
+            "ME GUSTA": "rahatsız edici ama hoşuma gidiyor demektir.",
             "Y U NO GUY": "Birşeyin neden mantıklı olmadığını söylemek için kullanılır."
             }
-
-word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
-
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print("Henüz benim sözlüğümde bu kelime yok.")
+            word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+            if word in meme_dict.keys():
+                print(meme_dict[word])
+            else:
+                print("Henüz benim sözlüğümde bu kelime yok.")
